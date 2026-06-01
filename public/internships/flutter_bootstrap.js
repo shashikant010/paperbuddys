972176fb2aa1d9491b68ef312203418279882446
shashikant010 <shashikantyadav0503@gmail.com>
2026-06-02 00:05:20 +0530

    updated dashboard

--- public/internships/flutter_bootstrap.js
+++ public/internships/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3973539667"
+    serviceWorkerVersion: "672686261"
   }
 });
