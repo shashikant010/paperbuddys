bb6547a0077524b3f541715fbbf658323c11dbbf
shashikant010 <shashikantyadav0503@gmail.com>
2026-08-12 15:09:08 +0530

    fixed class and route name issue in transport section

--- public/app/flutter_bootstrap.js
+++ public/app/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2476178305"
+    serviceWorkerVersion: "1162601763"
   }
 });
--- public/app/flutter_service_worker.js
+++ public/app/flutter_service_worker.js
@@ -59,7 +59,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "071fe2e722d11f59a638677d0ec7ac0f
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "ce53cb95128d3196f56614b50e5a1f5a",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "4742c5bea14b99ed33178aa472a4efb0",
+"flutter_bootstrap.js": "618115933aaabf550aac7604cfcc9295",
 "icons/Icon-192.png": "b3e5cfac2f582d50ad3c800e2d830649",
 "icons/Icon-512.png": "3eef89f9cc3bf40aaacf2e9811ceff21",
 "icons/Icon-maskable-192.png": "b3e5cfac2f582d50ad3c800e2d830649",
@@ -67,7 +67,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "071fe2e722d11f59a638677d0ec7ac0f
 "icons/paper_buddy_logo.png": "a24c22226f205f6885c9503330b0b50c",
 "index.html": "9dc12b9ce28776927a5a9787dc9ecad5",
 "/": "9dc12b9ce28776927a5a9787dc9ecad5",
-"main.dart.js": "b1dd6651e483af66217711d4288eab5d",
+"main.dart.js": "9e4a72b31b39b4a13406ba4ee2d37b57",
 "manifest.json": "11d6269e2c31e29d31b78035dee7be41",
 "version.json": "80dce41abdfad3cf248fe61b9b336ec2"};
 // The application shell files that are downloaded before a service worker can
